2a4200e6006c5c71e0abbddcd3e1c0149a0890c9
buster2605 <vipul26.h@gmail.com>
2016-06-26 10:52:38 +0530

    Choice added

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from polls.models import Question
-# Register your models here.
+from polls.models import Question, Choice
+
+
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
