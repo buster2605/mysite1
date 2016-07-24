5e8c5a436ac61fd9494165b498061b9300d92523
Vipul Gupta <vipul26.h@gmail.com>
2016-07-24 16:25:55 +0530

    Login and Register completed

--- polls/views.py
+++ polls/views.py
@@ -80,8 +80,12 @@ def register_page(request):
 	if request.method == 'POST':
 		form = RegistrationForm(request.POST)
 		if form.is_valid():
-			user = User.objects.create_user(username=form.cleaned_data['username'],password=form.cleaned_data['password1'],email=form.cleaned_data['email'])
-			return HttpResponseRedirect('/polls')
+			if form.cleaned_data['password1'] == form.cleaned_data['password2']:
+				user = User.objects.create_user(username=form.cleaned_data['username'],password=form.cleaned_data['password1'],email=form.cleaned_data['email'])
+				return HttpResponseRedirect('/polls')
+			else:
+				form = RegistrationForm()
+				return render(request, 'registration/register.html', {'error_message':'you have entered incorrect password', 'form':form})
 	form = RegistrationForm()
 	variables = RequestContext(request,{'form':form})
 	return render_to_response('registration/register.html', variables)
